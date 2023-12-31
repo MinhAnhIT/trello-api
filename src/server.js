358ea6c77faaefece210efca1b393ea9ddd618e0
MinhAnhIT <anhhoclaptrinh@gmail.com>
2023-12-31 15:58:21 +0700

    Deloy Production

--- src/server.js
+++ src/server.js
@@ -22,11 +22,21 @@ const SERVER_START = () => {
     // Middlewares xử lí lỗi tập trung
     app.use(errorHandlingMiddleware);
 
-    app.listen(env.APP_PORT, env.APP_HOST, () => {
-        // eslint-disable-next-line no-console
-        console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}`);
-    });
-
+    if (env.BUILD_MODE === 'production') {
+        app.listen(process.env.PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(
+                `Hello ${env.AUTHOR}, Back-End Server running at port:${process.env.PORT}`
+            );
+        });
+    } else {
+        app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
+            // eslint-disable-next-line no-console
+            console.log(
+                `Hello ${env.AUTHOR}, Back-End Server running at http://${env.LOCAL_DEV_APP_HOST}:${env.LOCAL_DEV_APP_PORT}`
+            );
+        });
+    }
     exitHook(() => {
         CLOSE_DB();
         console.log('Đã đóng kết nối database!');
